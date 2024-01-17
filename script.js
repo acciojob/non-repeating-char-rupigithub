cab7b0b95a3f9b5a6166bcc88a6e63db13dace21
rupigithub <117827907+rupigithub@users.noreply.github.com>
2024-01-17 15:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let l = str.length;
+	for(let i=0 i<l-1 i++){
+		if(str.charAt(i) != str.charAt(i+1)){
+				return i;
+			}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
